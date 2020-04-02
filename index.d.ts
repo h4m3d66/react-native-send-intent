4c65464c9701886ebaf2fa508a8058cf9541881c
hamed <hamed.87@hotmail.com>
2020-04-02 15:58:13 +0430

    add openAppWithDataActionEdit

--- index.d.ts
+++ index.d.ts
@@ -65,6 +65,7 @@ declare namespace SendIntentAndroid {
   const gotoHomeScreen: () => void
   const openApp: (packageName: string, extras: { [index: string]: string }) => void
   const openAppWithData: (packageName: string, dataUri: string, mimeType?: string, extras?: { [index: string]: string }) => Promise<boolean>
+  const openAppWithDataActionEdit: (packageName: string, dataUri: string, mimeType?: string, extras?: { [index: string]: string }) => Promise<boolean>
   const openChromeIntent: (dataUri: string) => Promise<boolean>
   const openDownloadManager: () => void
   const openFileChooser: (options: FileChooserOptions, title: string) => void
